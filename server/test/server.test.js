c591691735230af872c9bb4cee668805c35477d7
“kalpesh6295” <kalpesh6295@gmail.com>
2018-03-05 19:30:22 +0530

    testing update

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -68,28 +68,56 @@ describe('POST /Todos', ()=>{
     // });
     // });
 
-    describe('delete /todos/:id',()=>{
-        it('should delete todo doc',(done)=>{
+    // describe('delete /todos/:id',()=>{
+    //     it('should delete todo doc',(done)=>{
+    //        // var HexId =todo[0]._id.toHexString();
+    //        Todo.find().then((todos)=>{
+    //        var HexId=todos[0]._id.toHexString();
+    //        var length = todos.length;
+    //         request(app)
+    //         .delete(`/todos/${HexId}`)
+    //         .expect(200)
+    //         .expect((res)=>{
+    //            expect(res.body._id).toBe(HexId);
+               
+    //         })
+    //         .end((err,res)=>{
+    //             if(err){ return done(err)}
+    //             Todo.findById(HexId).then((todo)=>{
+    //                 console.log(todo);
+    //                 expect(todo).toNotExist();
+    //                 done();
+    //             })
+    //         });
+    //     });
+    // });
+    // });
+
+
+    describe('patch /todos/:id',()=>{
+        it('should update todo doc',(done)=>{
            // var HexId =todo[0]._id.toHexString();
            Todo.find().then((todos)=>{
            var HexId=todos[0]._id.toHexString();
-           var length = todos.length;
+           var text = todos[0].text;
             request(app)
-            .delete(`/todos/${HexId}`)
+            .patch(`/todos/${HexId}`)
+            .send({
+                text:"final testing for node todo api  completed",
+                completed:true
+
+            })
             .expect(200)
             .expect((res)=>{
                expect(res.body._id).toBe(HexId);
+               expect(res.body.text).toNotBe(text);
+               expect(res.body.completed).toBe(true);
+               expect(res.body.completedAt).toBeA('number');
                
             })
-            .end((err,res)=>{
-                if(err){ return done(err)}
-                Todo.findById(HexId).then((todo)=>{
-                    console.log(todo);
-                    expect(todo).toNotExist();
-                    done();
-                })
+            .end(done)
+                
             });
         });
     });
     });
-});
