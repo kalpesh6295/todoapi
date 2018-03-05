ee91c714c2cf631fcf9ac6115b70ef6f89a53b3d
“kalpesh6295” <kalpesh6295@gmail.com>
2018-03-05 20:07:04 +0530

    set up a seperate env for test and developement

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/todoapp');
+mongoose.connect(process.env.MONGODB_URI);
 mongoose.Promise=global.Promise;
 
 module.exports ={ mongoose};
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -3,33 +3,39 @@ const request = require('supertest');
 
 const {app}= require('./../server');
 const {Todo}= require('./../models/todo');
-
+var length;
+beforeEach((done=>{
+    Todo.find().then((todos)=>{
+        length=todos.length;
+        done();
+    })
+}))
 
 
 describe('POST /Todos', ()=>{
-    // it('should create a new todo item',(done)=>{
-    //     var text='test todo text';
-    //     var completed = true;
-    //     request(app)
-    //     .post('/Todos')
-    //     .send({text},{completed})
-    //     .expect(200)
-    //     .expect((res)=>{
-    //        console.log(res.body)
-    //         expect(res.body.text).toBe(text);
-    //     })
-    //     .end((err,res)=>{
-    //         if(err){
-    //             return done(err)}
-    //      Todo.find().then((todos)=>{
-    //          expect(todos.length).toBe(length+1);
-    //         expect(todos[length].text).toBe(text);
-    //          done();
+    it('should create a new todo item',(done)=>{
+        var text='test todo text';
+        var completed = true;
+        request(app)
+        .post('/Todos')
+        .send({text},{completed})
+        .expect(200)
+        .expect((res)=>{
+           console.log(res.body)
+            expect(res.body.text).toBe(text);
+        })
+        .end((err,res)=>{
+            if(err){
+                return done(err)}
+         Todo.find().then((todos)=>{
+             expect(todos.length).toBe(length+1);
+            expect(todos[length].text).toBe(text);
+             done();
              
-    //      })
-    //      .catch((e)=>done(e));
-    //     })
-    // })
+         })
+         .catch((e)=>done(e));
+        })
+    })
 
     // it('should not create a new todo item',(done)=>{
         
@@ -94,30 +100,30 @@ describe('POST /Todos', ()=>{
     // });
 
 
-    describe('patch /todos/:id',()=>{
-        it('should update todo doc',(done)=>{
-           // var HexId =todo[0]._id.toHexString();
-           Todo.find().then((todos)=>{
-           var HexId=todos[0]._id.toHexString();
-           var text = todos[0].text;
-            request(app)
-            .patch(`/todos/${HexId}`)
-            .send({
-                text:"final testing for node todo api  completed",
-                completed:true
+    // describe('patch /todos/:id',()=>{
+    //     it('should update todo doc',(done)=>{
+    //        // var HexId =todo[0]._id.toHexString();
+    //        Todo.find().then((todos)=>{
+    //        var HexId=todos[0]._id.toHexString();
+    //        var text = todos[0].text;
+    //         request(app)
+    //         .patch(`/todos/${HexId}`)
+    //         .send({
+    //             text:"final testing for node todo api  completed",
+    //             completed:true
 
-            })
-            .expect(200)
-            .expect((res)=>{
-               expect(res.body._id).toBe(HexId);
-               expect(res.body.text).toNotBe(text);
-               expect(res.body.completed).toBe(true);
-               expect(res.body.completedAt).toBeA('number');
+    //         })
+    //         .expect(200)
+    //         .expect((res)=>{
+    //            expect(res.body._id).toBe(HexId);
+    //            expect(res.body.text).toNotBe(text);
+    //            expect(res.body.completed).toBe(true);
+    //            expect(res.body.completedAt).toBeA('number');
                
-            })
-            .end(done)
+    //         })
+    //         .end(done)
                 
-            });
-        });
-    });
+    //         });
+    //     });
+    // });
     });
